54364518313a257fb2b980023597ffc2d4d2ef83
Veanty <plmulticraftpl@gmail.com>
2022-10-01 13:16:57 +0200

    Fix & Refactor: fix firebase initialization, imports. Project now uses updated react-firebase hooks

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -1,25 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
-//Firebase SDK
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
 
-//Firebase Hooks
-import { useAuthState } from "react-firebase-hooks-auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyA6Ru0mlhOrEJyIdBZ5UF1OlZAFd3kqdqc",
   authDomain: "super-chat-331c3.firebaseapp.com",
   projectId: "super-chat-331c3",
   storageBucket: "super-chat-331c3.appspot.com",
   messagingSenderId: "402521353323",
   appId: "1:402521353323:web:c0c6a6c891725d6fa8952e",
-});
+};
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
 
 function App() {
   const [user] = useAuthState(auth);
@@ -33,7 +32,7 @@ function App() {
 
 function SignIn() {
   const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
+    const provider = new auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   };
   return <button onClick={signInWithGoogle}>Sign in with Google</button>;
@@ -54,7 +53,22 @@ function SignOut() {
 function ChatRoom() {
   const messageRef = firestore.collection("messages");
   const query = messageRef.orderBy("createdAt").limit(25);
-  const [messages] = useCollectionData(query, { idField: "id" });
+  const [messages] = useCollection(query, { idField: "id" });
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((message) => (
+            <ChatMessage key={message.id} message={message}></ChatMessage>
+          ))}
+      </div>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const [text, uid] = props.message;
+  return <p>{text}</p>;
 }
 
 export default App;
