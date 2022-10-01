9414247a05c39e09c4f1b0cb9efdb355ff9fe5c6
Veanty <plmulticraftpl@gmail.com>
2022-10-01 12:31:18 +0200

    Feat: project config

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -9,6 +9,15 @@ import "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks-auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
+firebase.initializeApp({
+  apiKey: "AIzaSyA6Ru0mlhOrEJyIdBZ5UF1OlZAFd3kqdqc",
+  authDomain: "super-chat-331c3.firebaseapp.com",
+  projectId: "super-chat-331c3",
+  storageBucket: "super-chat-331c3.appspot.com",
+  messagingSenderId: "402521353323",
+  appId: "1:402521353323:web:c0c6a6c891725d6fa8952e",
+});
+
 function App() {
   return (
     <div className="App">
