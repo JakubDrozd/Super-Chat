0bceb9ecaec6b095410c045e1a9002e0ac204950
Veanty <plmulticraftpl@gmail.com>
2022-10-12 16:09:02 +0200

    Feat: add new test subjects

--- superchat/src/App.test.js
+++ superchat/src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Test to check if the App component loads properly
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
