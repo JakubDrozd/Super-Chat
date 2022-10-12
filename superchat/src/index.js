000227610dec0feff200e56f259933c8950b6120
Veanty <plmulticraftpl@gmail.com>
2022-10-12 16:10:09 +0200

    Refactor: modify the way of rendering main App component in the root of the webpage

--- superchat/src/index.js
+++ superchat/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Rendering JSX
+// Rendering App component in root
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
