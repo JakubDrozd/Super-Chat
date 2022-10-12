a8a3bdcef5a9a5bbd1b8ff353641e1089bfcf8a5
Veanty <plmulticraftpl@gmail.com>
2022-10-12 16:10:48 +0200

    Feat: add new performance measures for better webpage's optimazation

--- superchat/src/reportWebVitals.js
+++ superchat/src/reportWebVitals.js
@@ -1,3 +1,5 @@
+// Displays Vitals log to measure website's performance
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
